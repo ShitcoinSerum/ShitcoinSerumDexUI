be3b3f032d347bf5231fc67d25f99081ceddefa6
shitcoinserum <shitcoinserum@gmail.com>
2021-05-08 17:41:11 -0400

    init

---
+++ precache-manifest.f5a43625fdb532c6a160c26ef3aefd6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f42ba8398d4231dd828cf19f0a5bcf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad7e353bd2e00615ddfd",
+    "url": "/static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "f4ea1c6dfdce654c850a",
+    "url": "/static/css/main.9ba8d4ac.chunk.css"
+  },
+  {
+    "revision": "ad7e353bd2e00615ddfd",
+    "url": "/static/js/2.aea71690.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "/static/js/2.aea71690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ea1c6dfdce654c850a",
+    "url": "/static/js/main.f22f4024.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "41e6cff1a75c5aaebf96d2ad8dca5be4",
+    "url": "/static/media/shitcoinSerumLogoUI.41e6cff1.jpg"
+  }
+]);
